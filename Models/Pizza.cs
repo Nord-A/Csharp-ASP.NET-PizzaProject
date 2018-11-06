f18ccaf5a55e39cbd57f443c65da3f8cebb648c9
losoriol <luis.lutzen@gmail.com>
2018-11-06 14:11:44 +0100

    Add files via upload

---
+++ Models/Pizza.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PizzaProject.Models
+{
+    public class Pizza
+    {
+
+        
+        public int Id { get; set; }
+
+        public float Nr { get; set; }
+
+        public string Name { get; set; }
+        
+        public string Topping { get; set; }
+
+        public float Price { get; set; }
+
+       
+
+    }
+
+
+
+}
---
+++ Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PizzaProject.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string PassWord { get; set; }
+
+        public string Email { get; set; }
+
+        public string Username { get; set; }
+    }
+
+}
